0f0133e40a9eddffd6fae75c75a189be416df1b1
Elase Huang <elase.huang@outlook.com>
2022-05-16 01:13:38 +0800

    R hot dev server support history api fallback

--- config/webpack.dev.config.js
+++ config/webpack.dev.config.js
@@ -4,16 +4,17 @@ module.exports = () => {
   const rootFolder = process.cwd();
   const port = 12000;
   return {
-    mode: 'development',
+    mode: "development",
     output: {
-      path: path.join(rootFolder, 'dist'),
-      filename: 'js/[name].js',
+      path: path.join(rootFolder, "dist"),
+      filename: "js/[name].js",
       clean: true,
     },
-    devtool: 'eval-cheap-module-source-map',
+    devtool: "eval-cheap-module-source-map",
     cache: false,
     devServer: {
       port,
+      historyApiFallback: true,
       compress: true,
       open: true,
     },
